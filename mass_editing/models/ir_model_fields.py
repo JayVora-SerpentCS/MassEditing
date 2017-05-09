f45ad034f82913bb803305632eb0dd98def72f74
Serpent Consulting Services Pvt Ltd <serpentcs.lp@gmail.com>
2017-05-09 13:37:28 +0530

    Update ir_model_fields.py

--- mass_editing/models/ir_model_fields.py
+++ mass_editing/models/ir_model_fields.py
@@ -14,12 +14,13 @@ class IrModelFields(models.Model):
         for domain in args:
             if (len(domain) > 2 and
                 domain[0] == 'model_id' and
-                isinstance(domain[2], basestring) and
-                list(domain[2][1:-1])):
+                isinstance(domain[2], basestring) and list(domain[2][1:-1])):
                 model_domain += [('model_id', 'in',
                                   map(int, domain[2][1:-1].split(',')))]
             else:
                 model_domain.append(domain)
-        return super(IrModelFields, self).search(model_domain, offset=offset,
-                                                 limit=limit, order=order,
+        return super(IrModelFields, self).search(model_domain,
+                                                 offset=offset,
+                                                 limit=limit,
+                                                 order=order,
                                                  count=count)
