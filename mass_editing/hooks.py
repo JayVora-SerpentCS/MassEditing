3d8a096055c3fe4434453b94df33d5cf2faf5ae0
Vacha Trivedi <vacha.trivedi@serpentcs.com>
2017-11-10 16:13:05 +0530

    [FIX]mass_editing:Fixed travis error.

--- mass_editing/hooks.py
+++ mass_editing/hooks.py
@@ -4,5 +4,6 @@
 
 
 def uninstall_hook(cr, registry):
-    cr.execute("DELETE FROM ir_act_window WHERE res_model = 'mass.editing.wizard'")
+    cr.execute("DELETE FROM ir_act_window WHERE res_model = \
+    'mass.editing.wizard'")
     return True
