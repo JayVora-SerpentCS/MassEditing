7a597ad5c6ae2eaa1e143db1cb7348810b2da777
Serpent Consulting Services Pvt. Ltd <serpentcs.lp@gmail.com>
2018-01-30 17:43:08 +0530

    Update ir_model_fields.py

--- mass_editing/models/ir_model_fields.py
+++ mass_editing/models/ir_model_fields.py
@@ -11,14 +11,17 @@ class IrModelFields(models.Model):
     @api.model
     def search(self, args, offset=0, limit=0, order=None, count=False):
         model_domain = []
-        for domain in args:
-            if (len(domain) > 2 and domain[0] == 'model_id' and
-                    isinstance(domain[2], str) and
-                    list(domain[2][1:-1])):
-                model_domain += [('model_id', 'in',
-                                  [int(domain[2][1:-1].split(',')[0])])]
-            else:
-                model_domain.append(domain)
+        if not self.env.context.get('mass_edit', False):
+            model_domain = args
+        else:
+            for domain in args:
+                if (len(domain) > 2 and domain[0] == 'model_id' and
+                        isinstance(domain[2], basestring) and
+                        list(domain[2][1:-1])):
+                    model_domain += [('model_id', 'in',
+                                      map(int, domain[2][1:-1].split(',')))]
+                else:
+                    model_domain.append(domain)
         return super(IrModelFields, self).search(model_domain, offset=offset,
                                                  limit=limit, order=order,
                                                  count=count)
